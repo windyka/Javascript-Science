1ce8d046aa8f1126a33bccc336e7d9a89716a862
windyka <officialwindyka@gmail.com>
2019-01-14 03:46:03 +0700

    simple PIN validate 2

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function validatePIN(pin) {
-  if (
+  return (
     typeof pin === 'string' &&
     !~pin.indexOf('.') &&
     !isNaN(Number(pin)) &&
     (pin.length === 4 || pin.length === 6)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  );
+
   //return true or false
 }
 
