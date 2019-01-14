3693b733e39bd6f327f48db9526e9f7cdf7bf9d2
windyka <officialwindyka@gmail.com>
2019-01-14 14:58:05 +0700

    Jaden String Case (Solution 2)

--- index.js
+++ index.js
@@ -1,67 +1,25 @@
-function validatePIN(pin) {
-  return /^(\d{4}|\d{6})$/.test(pin);
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
 
-  //return true or false
-}
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
-// Test.describe('validatePIN', function() {
-//   Test.it(
-//     'should return False for pins with length other than 4 or 6',
-//     function() {
-//       Test.assertEquals(validatePIN('1'), false, "Wrong output for '1'");
-//       Test.assertEquals(validatePIN('12'), false, "Wrong output for '12'");
-//       Test.assertEquals(validatePIN('123'), false, "Wrong output for '123'");
-//       Test.assertEquals(
-//         validatePIN('12345'),
-//         false,
-//         "Wrong output for '12345'"
-//       );
-//       Test.assertEquals(
-//         validatePIN('1234567'),
-//         false,
-//         "Wrong output for '1234567'"
-//       );
-//       Test.assertEquals(
-//         validatePIN('-1234'),
-//         false,
-//         "Wrong output for '-1234'"
-//       );
-//       Test.assertEquals(
-//         validatePIN('1.234'),
-//         false,
-//         "Wrong output for '1.234'"
-//       );
-//       Test.assertEquals(
-//         validatePIN('-1.234'),
-//         false,
-//         "Wrong output for '-1.234'"
-//       );
-//       Test.assertEquals(
-//         validatePIN('00000000'),
-//         false,
-//         "Wrong output for '00000000'"
-//       );
-//     }
-//   );
+// Example:
 
-//   Test.it(
-//     'should return False for pins which contain characters other than digits',
-//     function() {
-//       Test.assertEquals(validatePIN('a234'), false, "Wrong output for 'a234'");
-//       Test.assertEquals(validatePIN('.234'), false, "Wrong output for '.234'");
-//     }
-//   );
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+// Note that the Java version expects a return value of null for an empty string or null.
 
-//   Test.it('should return True for valid pins', function() {
-//     Test.assertEquals(validatePIN('1234'), true, "Wrong output for '1234'");
-//     Test.assertEquals(validatePIN('0000'), true, "Wrong output for '0000'");
-//     Test.assertEquals(validatePIN('1111'), true, "Wrong output for '1111'");
-//     Test.assertEquals(validatePIN('123456'), true, "Wrong output for '123456'");
-//     Test.assertEquals(validatePIN('098765'), true, "Wrong output for '098765'");
-//     Test.assertEquals(validatePIN('000000'), true, "Wrong output for '000000'");
-//     Test.assertEquals(validatePIN('123456'), true, "Wrong output for '123456'");
-//     Test.assertEquals(validatePIN('090909'), true, "Wrong output for '090909'");
-//   });
-// });
+// FUNDAMENTALSSTRINGSARRAYS
 
-console.log(validatePIN('-1234'));
+// Solution 2 --------------------------------------------------------------------
+String.prototype.toJadenCase = function() {
+  return this.split(' ')
+    .map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+};
+// -------------------------------------------------------------------------------
+
+// Sample Test
+// var str = "How can mirrors be real if our eyes aren't real";
+// Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
